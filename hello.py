8b215af2acf0df180462bd65a5716f2af3d5bb63
Gleb Kozlutin <1932189348@bildung.daa.de>
2025-09-25 13:05:31 +0200

    hello_py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print ("Hello World")
+# start
+name = "Gleb"
+country = "Russland"
+age = "26"
+result = ""
+
+result = "Hello my name is "+name+ " i am " +age+" years old and i come from " +country+""
+print(result)
+#end
