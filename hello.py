84d277421373cc23957f3d7395ba0de8778b10f7
Gleb Kozlutin <1932189348@bildung.daa.de>
2025-09-25 13:06:40 +0200

    hello_py

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ country = "Russland"
 age = "26"
 result = ""
 
-result = "Hello my name is "+name+ " i am " +age+" years old and i come from " +country+""
+result = "Hello my name is "+name+" i am "+age+" years old and i come from "+country
 print(result)
 #end
